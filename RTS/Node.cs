99cd7d0e7e660da99c5917cb2e4594317377cb98
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-01-03 10:32:38 +0330

    Added Query and Node

---
+++ RTS/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS
+{
+    public class Node
+    {
+        public int Counter { get; set; }
+        public List<Query> Queries { get; set; }
+        public Node Parent { get; set; }
+        public Node RightNode { get; set; }
+        public Node LeftNode { get; set; }
+    }
+}
---
+++ RTS/Query.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS
+{
+    public class Query
+    {
+        public float Start { get; set; }
+        public float End { get; set; }
+        public int Threshold { get; set; }
+    }
+}
