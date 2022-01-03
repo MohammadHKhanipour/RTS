637a6d726f2b2fbd2cd6c9e3cf3f8eabeaf21f1b
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-01-03 19:17:32 +0330

    Added assumptions/explanation

--- RTS/Program.cs
+++ RTS/Program.cs
@@ -17,3 +17,23 @@ for (int i = 1; i <= queryCount; i++)
 }
 
 queries.OrderBy(q => q.Start);
+
+/*
+ * I think i'm supposed to consider start and end values for Nodes and for the first level start=end and for higher ones start!=end
+ * Then i can get all the start and end values of all queries distinct and sort them and then make the first level based on them then the -
+ * second level based on the first level and so on until we get to a root which covers all the values.
+ * I should keep track of each level so I can add two of the nodes as their parent's left and right child.
+ *             ┌────────┐
+               │        │
+               │ parent │
+           ┌──►│  node  │◄──┐
+           │   │        │   │
+           │   └────────┘   │
+           │                │
+        ┌──┴────┐       ┌───┴───┐
+        │       │       │       │
+        │ child │       │ child │
+        │ node  │       │ node  │
+        │       │       │       │
+        └───────┘       └───────┘
+ */
