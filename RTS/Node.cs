706bac1d003067d03f296c220ba5cfeeb87f6ebf
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-01-03 19:17:11 +0330

    Made properties nullable

--- RTS/Node.cs
+++ RTS/Node.cs
@@ -9,9 +9,9 @@ namespace RTS
     public class Node
     {
         public int Counter { get; set; }
-        public List<Query> Queries { get; set; }
-        public Node Parent { get; set; }
-        public Node RightChild { get; set; }
-        public Node LeftChild { get; set; }
+        public List<Query>? Queries { get; set; }
+        public Node? Parent { get; set; }
+        public Node? RightChild { get; set; }
+        public Node? LeftChild { get; set; }
     }
 }
